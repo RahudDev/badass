6c84ac61653253ce7695c6e209049e59bcd4e0c2
rahud <rahudnainggolan@gmail.com>
2024-05-19 16:33:34 +0700

    Updated to HashRouter for GitHub Pages

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -6,7 +6,7 @@ const LandingPage = () => {
   return (
     <div className="landing-page text-center">
       <div className="container d-flex flex-column align-items-center justify-content-center min-vh-100">
-        <h1 className="display-4 drop-in">Welcome to Cuan</h1>
+        <h1 className="display-4 drop-in">Welcome to Cuan badass</h1>
         <p className="lead drop-in delay-1">Earn rewards by completing simple tasks!</p>
         <Link to="/home" className="btn btn-primary btn-lg drop-in delay-2">Get Started</Link>
       </div>
