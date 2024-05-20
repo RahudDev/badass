4e5df34a5a101de2072111de23d9c5212bf08cda
rahud <rahudnainggolan@gmail.com>
2024-05-20 16:29:51 +0700

    Update React project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { HashRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
